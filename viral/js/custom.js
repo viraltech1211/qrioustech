6291c812334257133bcdcb486a277b426cbf01e0
Viral Patel <viral12111992@gmail.com>
2018-04-08 11:36:37 +0530

    portfolio

---
+++ viral/js/custom.js
@@ -0,0 +1,72 @@
+/*
+
+*	Void - Minimal Multipurpose Creative Portfolio Template
+* 	Author: OrbmobThemes
+* 	Copyright (C) 2018 Orbmob
+* 	This is a premium product available exclusively here : https://themeforest.net/user/orbmobthemes/portfolio
+
+
+*/
+
+
+
+
+
+
+
+/*====================================================
+
+
+	Table of Contents
+	
+	
+		01. Loading Screen 
+		
+		02. Lightbox
+
+	
+	
+====================================================*/
+
+
+
+jQuery(document).ready(function($) {
+	"use strict";
+
+	
+	
+/*======================
+
+	01. Loading Screen 
+
+========================*/
+
+
+
+	setTimeout(function(){
+		$('body').addClass('loaded');
+		$('h1').css('color','#222222');
+	}, 0);
+
+	
+	
+
+	
+/*======================
+
+	02. Lightbox 
+
+========================*/
+
+	
+	lightbox.option({
+      'resizeDuration': 200,
+      'wrapAround': true
+    });
+	
+	
+
+	
+
+	
+});
